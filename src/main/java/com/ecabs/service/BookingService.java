c2299fc4bbe744f6e9d5bffe97200fbf226170bb
Iurie Railean <blackrailean@gmail.com>
2020-03-11 23:35:54 +0100

    Creation of the basic Services of the application.

--- src/main/java/com/ecabs/service/BookingService.java
+++ src/main/java/com/ecabs/service/BookingService.java
@@ -1,7 +1,18 @@
 package com.ecabs.service;
 
+import com.ecabs.model.Booking;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public interface BookingService {
+    Booking getBookingById(final UUID id);
+
+    List<Booking> getAllBooking();
+
+    void savedBooking(final Booking booking);
+
+    void deleteBookingById(final UUID id);
 }
